3a42d3595702a05c2f77a8f3d76e321082ef8b36
Fouzy Robert Chehade <fouzy_chehade@icloud.com>
2022-07-26 17:12:45 +0200

    adds ThreadPoolExecution to the first download of the images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 from acnhanimaltracker.animals import AnimalHandler
 from acnhanimaltracker.GUI import Application
 from acnhanimaltracker.helper import is_folder_structure_intact
@@ -14,13 +15,15 @@ if __name__ == "__main__":
         animal_handler = AnimalHandler(root_directory)
         animal_handler.reset_animals(True, True, True)
         animal_list = animal_handler.load_animals()
-        animal_handler.download_images(animal_list)
+        for animal_type in animal_list:
+            with ThreadPoolExecutor(max_workers=50) as executor:
+                executor.map(animal_handler.download_animal_images, animal_type)
     else:
         animal_handler = AnimalHandler(root_directory)
         # animal_handler.reset_animals(True, True, True)
         animal_list = animal_handler.load_animals()
         # animal_handler.download_images(animal_list)
-    
+
     app = Application(animal_list, root_directory)
     app.mainloop()
     animal_handler.save_animals(animal_list)
