c50637e8511ef925424b88114a15ab8631748ec1
Fouzy Robert Chehade <fouzy_chehade@icloud.com>
2022-07-31 13:30:19 +0200

    removes redundant comment

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from acnhanimaltracker.animals import AnimalHandler
 from acnhanimaltracker.GUI import Application
 from acnhanimaltracker.helper import is_folder_structure_intact
 
-# On your first run make sure to also include line # and line # to download the data and images from the API-Endpoint and save it.
-# In the GUI you can go back to the Start Page when hitting the backspace key on your keyboard
-# You can click animals to select them as "caught"(green) or "uncaught"(red).
-# Saving of your progress is done when closing the app.
-
 if __name__ == "__main__":
     root_directory = os.path.dirname(__file__)
     if not is_folder_structure_intact(root_directory):
@@ -20,7 +15,7 @@ if __name__ == "__main__":
         # animal_handler.reset_animals(True, True, True)
         animal_list = animal_handler.load_animals()
         # animal_handler.download_images(animal_list)
-    
+
     app = Application(animal_list, root_directory)
     app.mainloop()
     animal_handler.save_animals(animal_list)
